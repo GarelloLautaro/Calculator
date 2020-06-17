e072fcbb07fbdcc16880f0d16b5764ad39b03aca
Lautaro Garello <lautarogarello99@gmail.com>
2020-06-17 12:11:10 -0300

    fix js

--- JS/app.js
+++ JS/app.js
@@ -1,62 +1,72 @@
-var resultado = document.getElementById("result");
-var cero = document.getElementById("n0")
-var uno = document.getElementById("n1")
-var dos = document.getElementById("n2")
-var tres = document.getElementById("n3")
-var cuatro = document.getElementById("n4")
-var cinco = document.getElementById("n5")
-var seis = document.getElementById("n6")
-var siete = document.getElementById("n7")
-var ocho = document.getElementById("n8")
-var nueve = document.getElementById("n9")
-var ce = document.getElementById("clear")
-var mas = document.getElementById("nA")
-var menos = document.getElementById("nS")
-var multiplicacion = document.getElementById("nM")
-var division = document.getElementById("nD")
-var operandoA;
-var operandoB;
-var operacion;
+var operandoA
+var operandoB
+var operacion
 
-cero.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "0"
+function getElement() {
+    resultado = document.getElementById("result");
+    cero = document.getElementById("n0")
+    uno = document.getElementById("n1")
+    dos = document.getElementById("n2")
+    tres = document.getElementById("n3")
+    cuatro = document.getElementById("n4")
+    cinco = document.getElementById("n5")
+    seis = document.getElementById("n6")
+    siete = document.getElementById("n7")
+    ocho = document.getElementById("n8")
+    nueve = document.getElementById("n9")
+    ce = document.getElementById("clear")
+    mas = document.getElementById("nA")
+    menos = document.getElementById("nS")
+    multiplicacion = document.getElementById("nM")
+    division = document.getElementById("nD")
 }
 
-uno.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "1"
+function onClick() {
+    cero.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "0"
+    }
+
+    uno.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "1"
+    }
+
+    dos.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "2"
+    }
+
+    tres.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "3"
+    }
+
+    cuatro.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "4"
+    }
+
+    cinco.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "5"
+    }
+
+    seis.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "6"
+    }
+
+    siete.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "7"
+    }
+
+    ocho.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "8"
+    }
+
+    nueve.onclick = function (e) {
+        resultado.textContent = resultado.textContent + "9"
+    }
+
+    ce.onclick = function (e) {
+        resetear()
+    }
 }
 
-dos.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "2"
-}
-
-tres.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "3"
-}
-
-cuatro.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "4"
-}
-
-cinco.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "5"
-}
-
-seis.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "6"
-}
-
-siete.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "7"
-}
-
-ocho.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "8"
-}
-
-nueve.onclick = function (e) {
-    resultado.textContent = resultado.textContent + "9"
-}
 
 function limpiar() {
     resultado.textContent = ""
@@ -72,8 +82,8 @@ function resetear() {
 function resetear() {
 
     resultado.textContent = ""
-    operandoA = 0;
-    operandoB = 0;
+    operandoA = 0
+    operandoB = 0
     operacion = ""
 }
 
@@ -97,6 +107,10 @@ function resolver() {
     resultado.textContent = res;
 }
 
-ce.onclick = function (e) {
+window.onload = function () {
+    getElement()
+    onClick()
+    limpiar()
     resetear()
+    resolver()
 }
