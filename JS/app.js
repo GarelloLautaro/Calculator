69db4d34c73635b604ec35142977b9f97fc5627a
Lautaro Garello <lautarogarello99@gmail.com>
2020-06-17 08:23:43 -0300

    first JS commit

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+var resultado = document.getElementById("result");
+var cero = document.getElementById("n0")
+var uno = document.getElementById("n1")
+var dos = document.getElementById("n2")
+var tres = document.getElementById("n3")
+var cuatro = document.getElementById("n4")
+var cinco = document.getElementById("n5")
+var seis = document.getElementById("n6")
+var siete = document.getElementById("n7")
+var ocho = document.getElementById("n8")
+var nueve = document.getElementById("n9")
+var ce = document.getElementById("clear")
+var mas = document.getElementById("nA")
+var menos = document.getElementById("nS")
+var multiplicacion = document.getElementById("nM")
+var division = document.getElementById("nD")
+var operandoA;
+var operandoB;
+var operacion;
